aef464611ac22faa11be21bafe0f9f89483c9b3c
venkatesh-pottabathula <103405063+venkatesh-pottabathula@users.noreply.github.com>
2022-05-18 16:56:44 +0530

    Add files via upload

---
+++ Entityframework/Entityframework/edproject/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using edproject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace edproject.Controllers
+{
+    [Route("api/abc")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        StudentDBContext context;
+        public StudentController(StudentDBContext con)
+        {
+            context = con;
+        }
+       [Route("add")]
+       [HttpGet]
+       public ActionResult addstudent(int id,string name)
+        {
+            Student stu = new Student();
+            stu.Id = id;
+            stu.Name = name;
+            context.Students.Add(stu);
+            context.SaveChanges();
+            return Ok("Success");
+        }
+        [Route("show")]
+       // [HttpGet]
+        public ActionResult showdetails()
+        {
+            Student stu = new Student();
+            var students = context.Students.ToList();
+            return Ok(students);
+        }
+    }
+}
