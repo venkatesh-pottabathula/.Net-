7738dccfd1929b6f4336f52098f76ebeb0e59044
venkatesh-pottabathula <103405063+venkatesh-pottabathula@users.noreply.github.com>
2022-05-18 16:54:05 +0530

    Add files via upload

---
+++ Entityframework/Entityframework/edproject/Controllers/DBNewContext.cs
@@ -0,0 +1,20 @@
+﻿using edproject.Models;
+using System;
+
+namespace edproject.Controllers
+{
+    internal class DBNewContext
+    {
+        public object Students { get; internal set; }
+
+        internal void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static implicit operator DBNewContext(StudentDBContext v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Entityframework/Entityframework/edproject/Models/StudentDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace edproject.Models
+{
+    public class StudentDBContext : DbContext
+    {
+        public StudentDBContext(DbContextOptions<StudentDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
